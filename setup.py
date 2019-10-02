ed496cd6f85005280e7a662bacee565e9d7de2b0
Tiffany Gray <KarateTD@yahoo.com>
2019-10-02 12:51:56 -0400

    updated Pipfile and setup.py for update with easy, medium , and hard questions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SnapshotAlyzer-3000",
-    version="1.0",
+    version="2.0",
     author="Tiffany Gray",
     author_email="KarateTD@yahoo.com",
     description="SnapshotAlyzer 3000",
@@ -11,7 +11,9 @@ setup(
     url="https://github.com/KarateTD/snapshotalyzer-3000",
     install_requires=[
         'click',
-        'boto3'
+        'boto3',
+        'datetime',
+        'botocore'
     ],
     entry_points='''
         [console_scripts]
